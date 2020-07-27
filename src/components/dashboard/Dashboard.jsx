0a524dc58e657af31875337c46e79be0452c4864
Lee <lsy63681@naver.com>
2020-07-27 15:05:41 +0900

    Create DashBoard

---
+++ src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React, { PureComponent } from "react";
+
+class Dashboard extends PureComponent {
+  render() {
+    return <div className="dashboard container"></div>;
+  }
+}
+
+export default Dashboard;

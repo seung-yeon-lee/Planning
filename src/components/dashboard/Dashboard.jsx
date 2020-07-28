1e8e240f06eece3ea245d0e9f20ce363cb4a1b76
Lee <lsy63681@naver.com>
2020-07-28 17:30:01 +0900

    Update Dashboard Component

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,8 +1,21 @@
 import React, { PureComponent } from "react";
+import Notifications from "./Notifications";
+import ProjectList from "../projects/ProjectList";
 
 class Dashboard extends PureComponent {
   render() {
-    return <div className="dashboard container"></div>;
+    return (
+      <div className="dashboard container">
+        <div className="row">
+          <div className="col s12 m6">
+            <ProjectList />
+          </div>
+          <div className="col s12 m5 offset-m1">
+            <Notifications />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 

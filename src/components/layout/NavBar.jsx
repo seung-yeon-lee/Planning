2b5dffdf35badbe1c5147764db3302b3df89510e
Lee <lsy63681@naver.com>
2020-07-28 17:30:13 +0900

    Update NavBar

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -7,8 +7,8 @@ const NavBar = () => {
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
-        <Link to="/" className="brand-logo">
-          PlanningApp
+        <Link to="/" className="brand-logo left">
+          Planning
         </Link>
         <SignInLinks />
         <SignOutLinks />

1afeb2476c134cd911a5bb9a7c62298a1438ef33
Lee <lsy63681@naver.com>
2020-07-29 17:13:13 +0900

    Connect Firebase

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,14 +1,18 @@
 import React, { PureComponent } from "react";
 import Notifications from "./Notifications";
 import ProjectList from "../projects/ProjectList";
+import { connect, ReactReduxContext } from "react-redux";
+import { firestoreConnect, firebaseConnect } from "react-redux-firebase";
+import { compose } from "redux";
 
 class Dashboard extends PureComponent {
   render() {
+    const { projects } = this.props;
     return (
       <div className="dashboard container">
         <div className="row">
           <div className="col s12 m6">
-            <ProjectList />
+            <ProjectList projects={projects} />
           </div>
           <div className="col s12 m5 offset-m1">
             <Notifications />
@@ -18,5 +22,24 @@ class Dashboard extends PureComponent {
     );
   }
 }
+//docs.react-redux-firebase.com/history/v3.0.0/docs/v3-migration-guide.html
 
-export default Dashboard;
+const mapStateToProps = (state) => {
+  return {
+    projects: state.project.projects,
+  };
+};
+// export default compose(
+//   firebaseConnect((props) => [{ path: "projects" }]),
+//   connect(mapStateToProps)
+// )(Dashboard);
+
+// export default compose(
+//   firebaseConnect(["projects"]),
+//   connect(mapStateToProps)
+// )(Dashboard);
+
+export default compose(
+  firestoreConnect([{ collection: "projects" }]),
+  connect(mapStateToProps)
+)(Dashboard);
--- src/index.js
+++ src/index.js
@@ -2,10 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./store/reducer/rootReducer";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import {
+  reactReduxFirebase,
+  getFirebase,
+  ReactReduxFirebaseProvider,
+} from "react-redux-firebase";
+import fbConfig from "./config/fbConfig";
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(
+      thunk.withExtraArgument({
+        getFirebase,
+        getFirestore,
+      })
+    ),
+    reduxFirestore(fbConfig),
+    reactReduxFirebase(fbConfig)
+  )
+);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );

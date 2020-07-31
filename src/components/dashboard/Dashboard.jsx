006d522d80ae70747eb6640a9d2f6ef57b7f02e2
Lee <lsy63681@naver.com>
2020-07-31 19:28:30 +0900

    Update DashBoard & Redirect

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,13 +1,15 @@
 import React, { PureComponent } from "react";
 import Notifications from "./Notifications";
 import ProjectList from "../projects/ProjectList";
-import { connect, ReactReduxContext } from "react-redux";
-import { firestoreConnect, firebaseConnect } from "react-redux-firebase";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
+import { Redirect } from "react-router-dom";
 
 class Dashboard extends PureComponent {
   render() {
-    const { projects } = this.props;
+    const { projects, auth } = this.props;
+    if (!auth.uid) return <Redirect to="/signin" />;
     return (
       <div className="dashboard container">
         <div className="row">
@@ -25,8 +27,10 @@ class Dashboard extends PureComponent {
 //docs.react-redux-firebase.com/history/v3.0.0/docs/v3-migration-guide.html
 
 const mapStateToProps = (state) => {
+  // console.log(state);
   return {
     projects: state.firestore.ordered.projects,
+    auth: state.firebase.auth, //login 안할 시 signin page만 표시하도록
   };
 };
 

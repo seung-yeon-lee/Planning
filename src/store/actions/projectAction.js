3ee3c9463dcc00f880f8058f4c4e88288a1fcc1e
Lee <lsy63681@naver.com>
2020-08-04 15:48:54 +0900

    Update ProjectAction&Reducer&profile

--- src/store/actions/projectAction.js
+++ src/store/actions/projectAction.js
@@ -3,12 +3,15 @@ export const CREATE_PROJECT = "project/CREATE_PROJECT";
 export const createProject = (data) => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     const firestore = getFirestore();
+    const profile = getState().firebase.profile;
+    const authId = getState().firebase.auth.uid;
     firestore
       .collection("projects")
       .add({
         ...data,
-        phoneNumber: "00000000",
-        nickName: "React",
+        phoneNumber: profile.phoneNumber,
+        nickName: profile.nickName,
+        authorId: authId,
         createdAt: new Date(),
       })
       .then(() => {
--- src/store/reducer/projectReducer.js
+++ src/store/reducer/projectReducer.js
@@ -1,14 +1,13 @@
 const initState = {
   projects: [{ id: "1", title: "Description", content: "FireBase" }],
-  entities: {},
 };
 
 const projectReducer = (state = initState, action) => {
   const { type, data } = action;
   switch (type) {
-    case "CREATE_PROJECT":
+    case "CREATE_PROJECT": {
       console.log("create", data);
-      return state;
+    }
     case "CREATE_ERR":
       console.log("Created Err");
       return state;

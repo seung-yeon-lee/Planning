92fe7e0fd973700aa2ca67634a32da410d3c8ff4
Lee <lsy63681@naver.com>
2020-08-04 15:34:43 +0900

    Update Dashboard & Connect firestore

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -8,7 +8,8 @@ import { Redirect } from "react-router-dom";
 
 class Dashboard extends PureComponent {
   render() {
-    const { projects, auth } = this.props;
+    console.log(notifications);
+    const { projects, auth, notifications } = this.props;
     if (!auth.uid) return <Redirect to="/signin" />;
     return (
       <div className="dashboard container">
@@ -17,24 +18,26 @@ class Dashboard extends PureComponent {
             <ProjectList projects={projects} />
           </div>
           <div className="col s12 m5 offset-m1">
-            <Notifications />
+            <Notifications notifications={notifications} />
           </div>
         </div>
       </div>
     );
   }
 }
-//docs.react-redux-firebase.com/history/v3.0.0/docs/v3-migration-guide.html
 
 const mapStateToProps = (state) => {
-  // console.log(state);
   return {
+    notifications: state.firestore.ordered.notifications,
     projects: state.firestore.ordered.projects,
-    auth: state.firebase.auth, //login 안할 시 signin page만 표시하도록
+    auth: state.firebase.auth,
   };
 };
 
 export default compose(
-  firestoreConnect([{ collection: "projects" }]),
-  connect(mapStateToProps)
+  connect(mapStateToProps),
+  firestoreConnect([
+    { collection: "projects", orderBy: ["createdAt", "desc"] },
+    { collection: "notifications", limit: 3, orderBy: ["time", "desc"] },
+  ])
 )(Dashboard);

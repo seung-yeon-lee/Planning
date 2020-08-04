f91d05a91175136dba5808996dcab653b3e31c7b
Lee <lsy63681@naver.com>
2020-08-04 15:33:54 +0900

    Update Notification

--- src/components/dashboard/Notifications.jsx
+++ src/components/dashboard/Notifications.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import moment from "moment";
 
-const Notifications = () => {
+const Notifications = (props) => {
+  const { notifications } = props;
+  console.log(notifications);
   return (
     <div>
-      <p>Notifications</p>
+      <div className="section">
+        <div className="card">
+          <div className="card-content">
+            <span className="card-title">
+              <b>Notifications</b>
+            </span>
+            <ul className="notifications">
+              {notifications &&
+                notifications.map((item) => {
+                  return (
+                    <li key={item.id}>
+                      <span className="pink-text">{item.user} 님의 </span>
+                      <span>{item.content}</span>
+                      <div className="grey-text note-date">
+                        {moment(item.time.toDate()).fromNow()}
+                      </div>
+                    </li>
+                  );
+                })}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

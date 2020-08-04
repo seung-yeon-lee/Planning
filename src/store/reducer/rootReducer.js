94e059d51aec5f37d6991ba2ceeea585847dd82a
Lee <lsy63681@naver.com>
2020-08-04 15:49:15 +0900

    Update IndexReducer

--- src/store/reducer/rootReducer.js
+++ src/store/reducer/rootReducer.js
@@ -2,11 +2,13 @@ import authReducer from "./authReducer";
 import projectReducer from "./projectReducer";
 import { combineReducers } from "redux";
 import { firestoreReducer } from "redux-firestore";
+import { firebaseReducer } from "react-redux-firebase";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   project: projectReducer,
-  firestore: firestoreReducer,
+  firestore: firestoreReducer, //db connect 기능
+  firebase: firebaseReducer, //auth 기능 구현
 });
 
 export default rootReducer;
